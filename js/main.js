a18259c17fede23c870bc372f1e9ba18664eff6f
SakaguchiMotonobu <sekenmunazanyo@gmail.com>
2020-06-14 22:24:01 +0900

    完了

--- js/main.js
+++ js/main.js
@@ -1,3 +1,66 @@
 "use strict";
 
-console.log("Hello World!");
+// console.log("Hello World!");
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`count ${i}`);
+// }
+
+// let hp = 100;
+
+// while (hp > 0) {
+//   console.log(`${hp} HP left!`);
+//   hp -= 15;
+// }
+
+// let hp = -50;
+
+// do {
+//   console.log(`${hp} HP left!`);
+//   hp -= 15;
+// } while (hp > 0);
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 3 === 0) {
+//     // continue;
+//     break;
+//   }
+//   console.log(i);
+// }
+
+// function showAd(message = "名産くだもの") {
+//   console.log("----------");
+//   console.log(`--- ${message} ---`);
+//   console.log("----------");
+// }
+
+// console.log("文文文文文");
+// console.log("文文文文文");
+// showAd("和歌山のみかん");
+// console.log("文文文文文");
+// console.log("文文文文文");
+// console.log("文文文文文");
+// console.log("文文文文文");
+// showAd();
+
+// function sum(a, b, c) {
+//   return a + b + c;
+// }
+
+// const sum = function (a, b, c) {
+//   return a + b + c;
+// };
+
+// const sum = (a, b, c) => a + b + c;
+
+// const total = sum(4, 5, 6) + sum(7, 8, 11);
+// console.log(total);
+
+// const double = (a) => a * 2;
+
+// console.log(double(8));
+
+{
+  const x = 100;
+  console.log(x);
+}
