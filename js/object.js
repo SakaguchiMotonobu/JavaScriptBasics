35e373810541c62f608fc6b0afd5cc5924e54d97
SakaguchiMotonobu <sekenmunazanyo@gmail.com>
2020-06-14 23:59:11 +0900

    詳解JavaScript オブジェクト編#05まで

--- js/object.js
+++ js/object.js
@@ -2,5 +2,21 @@
 
 {
   const scores = [80, 90, 40];
-  console.log(scores);
+  // console.log(scores[1]);
+
+  //   scores[2] = 44;
+  //   console.log(scores);
+
+  //   console.log(scores.length);
+
+  //   scores.push(60, 50);
+  //   scores.shift();
+  //   scores.unshift(99, 100);
+  //   scores.pop();(
+
+  scores.splice(1, 1, 40, 50);
+
+  for (let i = 0; i < scores.length; i++) {
+    console.log(`Scores ${i}: ${scores[i]}`);
+  }
 }
