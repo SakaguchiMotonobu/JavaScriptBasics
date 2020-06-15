71932288cc97b3ea18c4dc8267b3a4aaf011c693
SakaguchiMotonobu <sekenmunazanyo@gmail.com>
2020-06-15 23:34:33 +0900

    詳解JavaScript オブジェクト編#08まで

--- js/object.js
+++ js/object.js
@@ -3,22 +3,7 @@
 {
   const scores = [80, 90, 40, 70];
 
-  //   const [a, b, c, d] = scores;
-  //   console.log(a);
-  //   console.log(b);
-  //   console.log(c);
-  //   console.log(d);
-
-  //   const [a, b, ...c] = scores;
-  //   console.log(a);
-  //   console.log(b);
-  //   console.log(c);
-
-  let x = 30;
-  let y = 70;
-
-  [x, y] = [y, x];
-
-  console.log(x);
-  console.log(y);
+  scores.forEach((score, index) => {
+    console.log(`Score ${index}: ${score}`);
+  });
 }
