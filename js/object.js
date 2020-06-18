8c1d8d8f35b05cce5361ecdbcab8e77f468cc2a8
SakaguchiMotonobu <sekenmunazanyo@gmail.com>
2020-06-18 23:12:28 +0900

    詳解JavaScript オブジェクト編#20まで

--- js/object.js
+++ js/object.js
@@ -1,27 +1,5 @@
 "use strict";
 
 {
-  //   let x = [1, 2];
-  //   let y = [...x];
-  //   x[0] = 5;
-  //   console.log(x);
-  //   console.log(y);
-
-  //   const str = "hello";
-
-  //   console.log(str.length);
-  //   console.log(str.substring(2, 4));
-  //   console.log(str[1]);
-
-  //   const date = [2020, 6, 18];
-
-  //   console.log(date.join("/"));
-  //   console.log(date.join(""));
-
-  const time = "0:08:30";
-  //   console.log(time.split(":"));
-  const [h, m, s] = time.split(":"); //分割代入
-  console.log(h);
-  console.log(m);
-  console.log(s);
+  console.log(Math.floor(Math.random() * 6) + 1);
 }
