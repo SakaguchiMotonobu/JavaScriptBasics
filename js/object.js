c3052a2aaa9c915a887f60b9d2d468a9805c9c19
SakaguchiMotonobu <sekenmunazanyo@gmail.com>
2020-06-18 23:46:46 +0900

    詳解JavaScript オブジェクト編#22まで

--- js/object.js
+++ js/object.js
@@ -1,5 +1,10 @@
 "use strict";
 
 {
-  console.log(Math.floor(Math.random() * 6) + 1);
+  const d = new Date(2020, 10);
+  d.setHours(10, 20, 30);
+  d.setDate(31);
+  d.setDate(d.getDate() + 3);
+  console.log(d);
+  //   console.log(`${d.getMonth() + 1}月${d.getDate()}日`);
 }
