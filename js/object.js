b8fdb4b2cd1ead751e56887c01dd2cc1f1955fc8
SakaguchiMotonobu <sekenmunazanyo@gmail.com>
2020-06-20 16:20:25 +0900

    詳解JavaScript オブジェクト編 完了

--- js/object.js
+++ js/object.js
@@ -1,23 +1,50 @@
 "use strict";
 
 {
+  class Post {
+    constructor(text) {
+      this.text = text;
+      this.likeCount = 0;
+    }
+
+    show() {
+      console.log(`${this.text} - ${this.likeCount} likes`);
+    }
+    like() {
+      this.likeCount++;
+      this.show();
+    }
+    // 静的メソッド
+    // thisは使えない
+    static showInfo() {
+      console.log("Post class version 1.0");
+    }
+  }
+
+  class SponsoredPost extends Post {
+    constructor(text, sponsor) {
+      super(text);
+      this.sponsor = sponsor;
+    }
+
+    show() {
+      super.show();
+      console.log(`...sponsored by ${this.sponsor}`);
+    }
+    // 静的メソッド
+    // thisは使えない
+    // static showInfo() {
+    //   console.log("Post class version 1.0");
+    // }
+  }
+
   const posts = [
-    {
-      text: "JavaScriptの勉強中…",
-      likeCount: 0,
-      show() {
-        console.log(`${this.text} - ${this.likeCount}いいね`);
-      },
-    },
-    {
-      text: "プログラミング楽しい！",
-      likeCount: 0,
-      show() {
-        console.log(`${this.text} - ${this.likeCount}いいね`);
-      },
-    },
+    new Post("JavaScriptの勉強中…"),
+    new Post("プログラミング楽しい！"),
+    new SponsoredPost("３分動画でマスター", "ドットインストール"),
   ];
 
-  posts[0].show();
-  posts[1].show();
+  posts[2].show();
+  posts[2].like();
+  //   Post.showInfo();
 }
